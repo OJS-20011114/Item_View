e0d3b720d260532454499a9e13ce865c18f6b36f
JaeSeok Oh <74591216+OJS-20011114@users.noreply.github.com>
2020-11-18 13:20:20 +0900

    Wrapper_Update

--- src/main/java/io/jenkins/plugins/sample/BuildHis_Wra.java
+++ src/main/java/io/jenkins/plugins/sample/BuildHis_Wra.java
@@ -30,10 +30,11 @@ public class BuildHis_Wra extends BuildWrapper {
                 //중요 포인트(1)
                 return new Environment()
                 {
-                    public boolean tearDown()throws IOException, InterruptedException
-                    {
-                        return true;
-                    }
+			return true;
+//                     public boolean tearDown()throws IOException, InterruptedException
+//                     {
+//                         return true;
+//                     }
                         
                     
                 }; 
