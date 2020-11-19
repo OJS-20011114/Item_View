874137849a82606564a16c9264ca249d501bf98b
OJS-20011114 <74591216+OJS-20011114@users.noreply.github.com>
2020-11-19 16:45:27 +0900

    Used_Script

---
+++ src/main/resources/io/jenkins/plugins/sample/View_MainPanel_text/SCrip_S.js
@@ -0,0 +1,5 @@
+for(;i >= 1;i--)
+                {
+                    document.write("<tr><td>",i,"</td><td>2020.11.10 오후 7:11","</td><td>");
+                    document.write("</td></tr>");
+                }
