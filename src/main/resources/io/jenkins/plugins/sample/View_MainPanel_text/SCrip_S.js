c2b3f64fdb6b4bd0bb57667a13979c1858f753ec
OJS-20011114 <74591216+OJS-20011114@users.noreply.github.com>
2020-11-20 12:08:42 +0900

    Send

--- src/main/resources/io/jenkins/plugins/sample/View_MainPanel_text/SCrip_S.js
+++ src/main/resources/io/jenkins/plugins/sample/View_MainPanel_text/SCrip_S.js
@@ -1,3 +1,4 @@
+var i = document.getElementById('GET_BUILD_NUM').value;
 for(;i >= 1;i--)
                 {
                     document.write("<tr><td>",i,"</td><td>2020.11.10 오후 7:11","</td><td>");
